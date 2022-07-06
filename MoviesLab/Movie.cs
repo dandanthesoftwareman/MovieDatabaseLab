caa5220d485668287a7f1568bc5ee48e88ad0d3d
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-07-06 17:46:44 -0400

    First Commit

---
+++ MoviesLab/Movie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesLab
+{
+    internal class Movie
+    {
+        //properties
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public Movie(string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+        public static bool GetContinue()
+        {
+            //default value, will eventually be replaced
+            bool result = true;
+            //check if user wants to continue
+            while (true)
+            {
+                Console.WriteLine("Would you like to browse more? y/n");
+                string choice = Console.ReadLine().ToLower().Trim();
+
+                //check user input
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That result was invalid. Try again.");
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MoviesLab/Program.cs
@@ -0,0 +1,42 @@
+﻿using MoviesLab;
+bool runProgram = true;
+List<Movie> MovieList = new List<Movie>()
+{
+    new Movie("Star Wars I", "scifi"),
+    new Movie("Star Wars II", "scifi"),
+    new Movie("Star Wars III", "scifi"),
+    new Movie("Saw I", "horror"),
+    new Movie("Scream", "horror"),
+    new Movie("Finding Nemo", "animated"),
+    new Movie("How To Train Your Dragon", "animated"),
+    new Movie("Les Mis", "drama"),
+    new Movie("House: The Movie", "drama"),
+    new Movie("Rugrats Go to Paris", "animated")
+};
+
+Console.WriteLine("Welcome to the movie store, a thing that still totally exists.");
+
+while (runProgram)
+{
+    while (true)
+    {
+    Console.WriteLine("What kind of movie are you looking for? We carry animated, scifi, horror, and drama films.");
+    string genre = Console.ReadLine().ToLower().Trim();
+        if (MovieList.Where(m => m.Category == genre).Count() > 0)
+        {
+            foreach (Movie m in MovieList)
+            {
+                if (genre == m.Category)
+                {
+                    Console.WriteLine(m.Title);
+                }
+            }
+            break;
+        }
+        else
+        {
+            Console.WriteLine("We don't carry that genre");
+        }
+    }
+    runProgram = Movie.GetContinue();
+}
